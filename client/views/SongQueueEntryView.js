59cc23e245ffc564c4685b06ac0aea810908c9b1
Charles Crame <ccrame@gmail.com>
2015-07-15 18:27:17 -0700

    Attempted SongQueue stuff

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,33 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events:{
+    'click': function(){ // Definitely not click, but definitely don't know yet
+
+    }
+  },
+
+  render: function(){
+    return this.#el.html(this.template(this.model.attributes));
+  }
 });
+
+
+// tagName: 'tr',
+
+//   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+//   events: {
+//     'click': function() {
+//       this.model.enqueue();
+//       this.model.play();
+//     }
+//   },
+
+//   render: function(){
+//     return this.$el.html(this.template(this.model.attributes));
+//   }
